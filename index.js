d51a08bce355176fd88fced6e241a43124195d4f
dangphuc2611 <cuccukgiontan2611@gmail.com>
2022-05-18 01:51:39 +0700

    button

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 console.log('run')
 
+window.onload = function hiddenImage() {
+  let imageResult = document.getElementById('image-result')
+  imageResult.style.visibility = 'hidden';
+}
+
 function showAlert() {
     alert('dangphuc')
   }
+function showResult(){
+  let imageResult = document.getElementById('image-result')
+  imageResult.style.visibility = 'visible';
+}
